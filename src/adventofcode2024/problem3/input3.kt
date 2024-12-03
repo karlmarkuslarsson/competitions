48b6faca619e5ceb3e39885e8fea2fde617eebc5
Markus Larsson <markus@Markuss-MacBook-Pro.local>
2024-12-03 19:22:09 +0100

    problem 3 solution

--- src/adventofcode2024/problem3/input3.kt
+++ src/adventofcode2024/problem3/input3.kt
@@ -1,4 +1,5 @@
 package adventofcode2024.problem3
 
 const val input3 = """"""
-const val input3test = """"""
+const val input3test = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+"""
--- src/adventofcode2024/problem3/problem3.kt
+++ src/adventofcode2024/problem3/problem3.kt
@@ -1,13 +1,32 @@
 package adventofcode2024.problem3
 
+import java.math.BigInteger
+
 fun main() {
     solveA()
     solveB()
 }
 
 fun solveA() {
-    
+    var sum = BigInteger.ZERO
+    val allMul = input3test.split("mul(")
+    allMul.forEach {
+        val split = it.split(",")
+        try {
+            val firstStr = split[0]
+            if (firstStr.length in 1..3 && firstStr[0] != '0') {
+                val first = firstStr.toLong()
+                val secondStr = split[1].split(")")[0]
+                if (secondStr.length in 1..3 && secondStr[0] != '0') {
+                    val second = secondStr.toLong()
+                    sum += (BigInteger.valueOf(first).multiply(BigInteger.valueOf(second)))
+                }
+            }
+        } catch (e: Exception) {
 
+        }
+    }
+    println(sum)
 }
 
 fun solveB() {
